511eaa88bd48408c41985744223fc5c4a14725a8
yieniggu <simpertiguebastian@gmail.com>
2025-07-19 18:28:53 -0400

    feat(server): setup express server with dotenv and cors

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_, res) => res.send('API is running'));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
